4f34499e0d1212e09d9ce1e87d844877f7f92a53
jcervoni <jcervoni@student.42.fr>
2022-04-13 16:15:17 +0200

    UPDATE MAIN.C :
    Update main.c, testing readline's return;
    
    Sur la branche jmc/parsing
    Modifications qui seront validées :
    	modifié :         main.c

--- sources/main.c
+++ sources/main.c
@@ -6,22 +6,25 @@
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 09:18:40 by aweaver           #+#    #+#             */
-/*   Updated: 2022/04/12 18:05:26 by jcervoni         ###   ########.fr       */
+/*   Updated: 2022/04/13 15:45:36 by jcervoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <readline/readline.h>
 
 int	main(int ac, char *av[], char *envp[])
 {
 	(void)envp;
 	(void)ac;
 	(void)av;
-	char * test;
+	char 	*test;
+	t_arg	*verif;
 
 	test = readline("Test readline :");
-	printf("%s", test);
-	//free(test);
+	verif = ft_get_args(test);
+	ft_set_token(verif);
+	printf("%s\n", verif->token);
+	free(test);
+	ft_cleararg(verif);
 	return (0);
 }
