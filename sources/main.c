030c5cabd1d3806dd0c5d18dfef0c1d708a2cd60
Jean-michel CERVONI <jcervoni@student.42.fr>
2022-04-14 16:58:28 +0200

    UPDATE MAIN.C :
    Update main.c : normed, still testing file;
    
    On branch jmc/parsing
    Changes to be committed:
    	modified:   sources/main.c

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 09:18:40 by aweaver           #+#    #+#             */
-/*   Updated: 2022/04/14 10:05:34 by jcervoni         ###   ########.fr       */
+/*   Updated: 2022/04/14 16:45:39 by jcervoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	main(int ac, char *av[], char *envp[])
 {
+	char	*test;
+	t_arg	*verif;
+
 	(void)envp;
 	(void)ac;
 	(void)av;
-	char 	*test;
-	t_arg	*verif;
-
 	test = readline("Test readline :");
 	verif = ft_get_args(test);
 	ft_set_token(verif);
@@ -28,7 +28,5 @@ int	main(int ac, char *av[], char *envp[])
 		printf("content = %s,  token = %s\n", verif->content, verif->token);
 		verif = verif->next;
 	}
-//	free(test);
-	//ft_cleararg(verif);
 	return (0);
 }
