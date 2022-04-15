0d53c9a13f29d0594d7a1b4adc4050c4aada7cc1
Adrian WEAVER <aweaver@student.42.fr>
2022-04-15 15:07:29 +0200

    ADD FT_ENV_TO_LIST.C UPTADE MINISHELL_STRUCT.H:
    Adding file ft_env_to_list in ./utils/ the purpose of the function
    that will be built in this file is to transform the env in a list.
    
    Function is not added in this commit and is yet to be made.
    
    Changes to be committed:
    	modified:   includes/minishell_struct.h
    	new file:   sources/utils/ms_env_to_list.c

--- includes/minishell_struct.h
+++ includes/minishell_struct.h
@@ -6,7 +6,7 @@
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 14:10:20 by jcervoni          #+#    #+#             */
-/*   Updated: 2022/04/12 16:44:00 by jcervoni         ###   ########.fr       */
+/*   Updated: 2022/04/15 15:04:49 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,17 @@ typedef struct s_arg
 	struct s_arg	*next;
 }	t_arg;
 
-typedef struct	s_mlc
+typedef struct s_mlc
 {
 	void			*ptr;
 	struct s_mlc	*next;
 }	t_mlc;
+
+typedef struct s_env
+{
+	char			*name;
+	char			**content;
+	struct s_env	*next;
+}	t_env;	
 
 #endif
