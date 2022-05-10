4772701401f95682bbdd2f9eafac8205590ef4cf
Adrian WEAVER <aweaver@student.42.fr>
2022-05-10 17:46:42 +0200

    FIX FT_FREE_ENV:
    Fixing free env function, now working as expected without leaks.
    The ms_env_to_list function still needs to be worked out for potential
    malloc malfunctions
    
    Changes to be committed:
    	modified:   sources/utils/ms_free_env.c

--- sources/utils/ms_free_env.c
+++ sources/utils/ms_free_env.c
@@ -6,12 +6,13 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:27:33 by aweaver           #+#    #+#             */
-/*   Updated: 2022/05/09 19:20:41 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/05/10 17:37:40 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/* DEPRECATED */
 void	ft_free_double_array(char **str)
 {
 	int		i;
@@ -22,37 +23,36 @@ void	ft_free_double_array(char **str)
 		free(str[i]);
 		i++;
 	}
-	free(str);
+	printf("str addr = %p\n", str);
 }
 
 void	ft_env_delone(t_env *env, void (*del)(void *))
 {
-	//int	i;
-//
-	//i = 0;
+	int	i;
+
+	i = 0;
 	del(env->name);
 	if (env->content)
-		ft_free_double_array(env->content);
-	//while ((env->content[i]))
-	//{
-		//del(env->content[i]);
-		//i++;
-	//}
-	//del(env->content);
+	{
+		while ((env->content[i]))
+		{
+			del(env->content[i]);
+			i++;
+		}
+		del(env->content);
+	}
 	del(env);
 }
 
-void	ft_free_env(t_env **env_start)
+void	ft_free_env(t_env *env_start)
 {
-	t_env	**save_env_start;
 	t_env	*tmp;
 
-	save_env_start = env_start;
-	while (*env_start)
+	while (env_start)
 	{
-		tmp = *env_start;
-		*env_start = (*env_start)->next;
+		tmp = env_start;
+		env_start = env_start->next;
+		printf("NAME=%s\n", tmp->name);
 		ft_env_delone(tmp, free);
 	}
-	free(save_env_start);
 }
