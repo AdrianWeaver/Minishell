1420ca92f1343fc8e3eab092994ce2fd265634d8
Jean-michel CERVONI <jcervoni@student.42.fr>
2022-05-11 13:45:52 +0200

    UPDATE STRUCT.H / PARSING.H / MAIN.C :
    Update .h with new parsing functions and reorganization;
    Update main.c with new parsing test;

--- builtins/ms_echo.c
+++ builtins/ms_echo.c
@@ -1,36 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell_struct.h                                 :+:      :+:    :+:   */
+/*   ms_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/12 14:10:20 by jcervoni          #+#    #+#             */
-/*   Updated: 2022/05/10 18:24:47 by jcervoni         ###   ########.fr       */
+/*   Created: 2022/05/10 16:51:25 by jcervoni          #+#    #+#             */
+/*   Updated: 2022/05/10 17:42:03 by jcervoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_STRUCT_H
-# define MINISHELL_STRUCT_H
+#include "minishell"
 
-typedef struct s_arg
+int	ft_echo(char *to_print)
 {
-	int				token;
-	char			*content;
-	struct s_arg	*next;
-}	t_arg;
+	int	i;
+	int	end;
 
-typedef struct s_mlc
-{
-	void			*ptr;
-	struct s_mlc	*next;
-}	t_mlc;
-
-typedef struct s_env
-{
-	char			*name;
-	char			*content;
-	struct s_env	*next;
-}	t_env;	
-
-#endif
+	i = 0;
+	while (to_print[i] != '\0')
+	{
+		if (to_print[0] == '"')
+		{
+			end = 1;
+			while (to_print[++i] != '"' && to_print[i])
+			{
+				end++;
+			}
+		}
+	return (0);
+}
