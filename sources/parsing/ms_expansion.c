3cccc0691aa45cafc1fd07ba7385ae52bbddf86f
Jean-michel CERVONI <jcervoni@student.42.fr>
2022-05-12 18:12:45 +0200

    UPDATE parsing files.c :
    Update ms_expansion : creation of expansion functions, WIP;
    Update ms_parse_type.c : manage dquotes and erase them;

--- sources/parsing/ms_expansion.c
+++ sources/parsing/ms_expansion.c
@@ -6,51 +6,151 @@
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 15:00:46 by jcervoni          #+#    #+#             */
-/*   Updated: 2022/05/11 17:43:22 by jcervoni         ###   ########.fr       */
+/*   Updated: 2022/05/12 18:04:37 by jcervoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	**ft_reasy_to_expand(t_arg *arg)
+int	ft_check_var(char *str)
 {
-	char	**temp;
-	int		i;
-
-	i = 1;
-	while(arg->content[i] != '\0')
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	if (str[i] == '$' && (ft_isalnum(str[i + 1]) == 1 || str[i + 1] == '_'))
 	{
-		if (arg->content[i] == '$')
+		i++;
+		while (str[i] && (ft_isalnum(str[i]) == 1 || str[i] == '_'))
+		{
 			i++;
+			j++;
+		}
+		return (j);
 	}
-	if (i > 1)
-	{
-		temp = malloc(sizeof(char *) * i + 2);
-		if (temp == NULL)
-			return (NULL);
-	}
+	return (0);
 }
 
-// void ft_expand(t_arg *arg, t_env *env, char ** temp)
-// {
-// 	int	i;
-// 	int	j;
-// 	int	t;
+char	**ft_count_expand(t_arg *arg)
+{
+	char	**pieces;
+	int		exp;
+	int		i;
+	int		st;
 
-// 	i = 0;
-// 	j = 0;
-// 	t = 0;
-// 	while (arg->content[i] != '\0')
-// 	{
-// 		if (arg->content[i] == '$' && (ft_isalnum(arg->content[i + 1]) == 1
-// 			|| arg->content[i + 1] == '_'))
-// 		{
-// 			temp[t] = ft_substr(arg->content, j, i - j);
-// 			t++;
-// 			j = i + 1;
-// 			while 
-// 		}
-// 		i++;
-		
-// 	}
-// }
+	exp = 0;
+	i = 0;
+	st = i;
+	while (arg->content[i] != '\0')
+	{
+		while (arg->content[i] != '\0' &&ft_check_var(&arg->content[i]) < 1)
+			i++;
+		if (i > st)
+			exp++;
+		if (arg->content[i] == '$' && (ft_isalnum(arg->content[i + 1]) == 1
+				|| arg->content[i + 1] == '_'))
+			exp++;
+		i += ft_check_var(&arg->content[i]) + 1;
+	}
+	if (exp > 0)
+	{
+		pieces = malloc(sizeof(char *) * exp + 1);
+		if (!pieces)
+			return (NULL);
+		pieces[exp] = NULL;
+	}
+	return (pieces);
+}
+
+int	ft_expand_size(char *str, t_env *env)
+{
+	t_env	*temp;
+	char	*str_name;
+	int		i;
+	int		len;
+
+	temp = env;
+	i = 0;
+	len = 0;
+	while (str[i] && (ft_isalnum(str[i]) == 1 || str[i] == '_'))
+		i++;
+	str_name = ft_substr(str, 0, i);
+	if (!str_name)
+		return (-1);
+	while (temp != NULL)
+	{
+		if (ft_strcmp(str_name, temp->name) == 0)
+		{
+			len = ft_strlen(temp->content[0]) - ft_strlen(temp->name);
+			break ;
+		}
+		temp = temp->next;
+	}
+	free(str_name);
+	return (len);
+}
+
+char	*ft_get_expanded(char *str, t_env *env)
+{
+	t_env	*temp;
+	char	*var_ret;
+	char	*name;
+	int		len;
+
+	len = 0;
+	temp = env;
+	var_ret = NULL;
+	while (ft_isalnum(str[len]) == 1 || str[len] == '_')
+		len++;
+	name = ft_substr(str, 0, len);
+	while (temp != NULL)
+	{
+		if (ft_strcmp(temp->name, name) == 0)
+		{
+			var_ret = ft_strdup(temp->content[0]);
+			break ;
+		}
+		temp = temp->next;
+	}
+	return (var_ret);
+}
+
+void	ft_get_final_string(t_arg *arg, char **pieces, t_env *env)
+{
+	char	*env_content;
+	int		i;
+	int		j;
+	int		st;
+
+	i = 0;
+	j = 0;
+	st = i;
+	while (arg->content[i] != '\0')
+	{
+		while (arg->content[i + 1] != '\0' && ft_check_var(&arg->content[i]) < 1)
+			i++;
+		if (arg->content[i] != '\0')
+		{
+			if (ft_check_var(&arg->content[i]) > 0)
+				env_content = ft_get_expanded(&arg->content[i + 1], env);
+			if (i > st)
+			{
+				pieces[j] = ft_substr(arg->content, st, i - st);
+				j++;
+			}
+			if (env_content != NULL)
+			{
+				pieces[j] = ft_strdup(env_content);
+				free(env_content);
+				j++;
+			}
+			i+= ft_check_var(&arg->content[i]) + 1;
+			st = i;
+		}
+	}
+	for (int x = 0; pieces[x] != NULL;x++)
+	{
+		printf("pieces[%d] = %s\n", x, pieces[x]);
+	}
+}
--- sources/parsing/ms_parse_type.c
+++ sources/parsing/ms_parse_type.c
@@ -6,7 +6,7 @@
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 18:09:57 by jcervoni          #+#    #+#             */
-/*   Updated: 2022/05/11 17:12:37 by jcervoni         ###   ########.fr       */
+/*   Updated: 2022/05/12 16:28:31 by jcervoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ int	ft_remove_dquotes(t_arg *arg)
 		{
 			if (i == 0)
 				st = 1;
-			if (i > st)
+			else if (i > st)
 			{
 				temp = ft_substr(arg->content, st, i - st);
 				to_copy = ft_strjoin(to_copy, temp);
@@ -77,10 +77,11 @@ int	ft_remove_dquotes(t_arg *arg)
 	return (0);
 }
 
-int	ft_check_dquotes(t_arg *arg)
+int	*ft_count_dquotes(t_arg *arg)
 {
 	int	i;
 	int	dq;
+	int	*dq_nbr;
 
 	i = 0;
 	dq = 0;
@@ -90,7 +91,49 @@ int	ft_check_dquotes(t_arg *arg)
 			dq++;
 		i++;
 	}
-	if ((dq != 0 && dq % 2 != 0) || dq == 0)
+	if (dq != 0 && dq % 2 != 0)
+		return (NULL);
+	dq_nbr = calloc(sizeof(int), dq + 1);
+	if (!dq_nbr)
+		return (NULL);
+	dq_nbr[0] = dq;
+	return (dq_nbr);
+}
+
+void	ft_set_final_dq_index(t_arg *arg, int *dq_nbr, t_env *env)
+{
+	int		i;
+	int		j;
+	
+	i = 0;
+	j = 1;
+	while (arg->content[i] != '\0')
+	{
+		if (arg->content[i] == '"')
+		{
+			dq_nbr[j] += i;
+			printf("quote a l'index :%d\n", dq_nbr[j]);
+			j++;
+		}
+		else if (arg->content[i] == '$' && (ft_isalnum(arg->content[i + 1]) == 1
+			|| arg->content[i + 1] == '_'))
+			{
+				dq_nbr[j] += ft_expand_size(&arg->content[i + 1], env) - 1;
+			}
+		i++;	
+	}
+}
+
+int	ft_test(t_arg *arg, t_env *env)
+{
+	int		*dq;
+	char	**pieces;
+	
+	pieces = ft_count_expand(arg);
+	dq = ft_count_dquotes(arg);
+	if (!dq || !pieces)
 		return (-1);
+	ft_set_final_dq_index(arg, dq, env);
+	ft_get_final_string(arg, pieces, env);
 	return (0);
 }
