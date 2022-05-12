1949de2c9d5ab6e90ecaeb19178e428386b1610f
Adrian WEAVER <aweaver@student.42.fr>
2022-05-12 15:21:33 +0200

    UPDATE MS_FREE_ENV.C:
    Adding comments in ms_free_env.c
    
    Changes to be committed:
    	modified:   ms_free_env.c

--- sources/utils/ms_free_env.c
+++ sources/utils/ms_free_env.c
@@ -6,13 +6,17 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:27:33 by aweaver           #+#    #+#             */
-/*   Updated: 2022/05/12 14:38:48 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/05/12 15:21:20 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/* DEPRECATED */
+/* ************************************************************************ */
+/*	ACT : frees a char **str											    */
+/*	ARG : a char **str														*/
+/*	RET : void																*/
+/* ************************************************************************ */
 void	ft_free_double_array(char **str)
 {
 	int		i;
@@ -26,6 +30,11 @@ void	ft_free_double_array(char **str)
 	printf("str addr = %p\n", str);
 }
 
+/* ************************************************************************ */
+/*	ACT : deletes an element (t_env) of a t_env list breaking the list		*/
+/*	ARG : a t_env and a function returning void and taking a void pointer	*/
+/*	RET : void																*/
+/* ************************************************************************ */
 void	ft_env_delone(t_env *env, void (*del)(void *))
 {
 	int	i;
@@ -44,6 +53,11 @@ void	ft_env_delone(t_env *env, void (*del)(void *))
 	del(env);
 }
 
+/* ************************************************************************ */
+/*	ACT : deletes cleanly the env											*/
+/*	ARG : the first element of the env										*/
+/*	RET : void																*/
+/* ************************************************************************ */
 void	ft_free_env(t_env *env_start)
 {
 	t_env	*tmp;
