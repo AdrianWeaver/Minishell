4c375f36e6679de5a56995b221c4e074588328dc
Adrian WEAVER <aweaver@student.42.fr>
2022-05-12 18:15:50 +0200

    WIP MS_UNSET.C:
    Heavy WIP working on ms_unset this is really just a beginning.

---
+++ sources/utils/ms_unset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_unset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 17:58:52 by aweaver           #+#    #+#             */
+/*   Updated: 2022/05/12 18:14:35 by aweaver          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_env *ft_find_env_elem(t_env *env_list, char *name)
+{
+	while (env_list)
+	{
+		if (ft_strcmp(env_list->name, name) == 0)
+			return (env_list);
+		env_list = env_list->next;
+	}
+}
+
+t_env	*ft_unset(t_env *env_list, char *name)
+{
+	
+}
