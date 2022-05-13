fee6e242bee029fa390b35244f22a47bd370e677
Adrian WEAVER <aweaver@student.42.fr>
2022-05-13 16:13:26 +0200

    ADD MS_UNSET.C:
    Add ft_unset, replicates the behaviour of unset builtin.
    Add ft_cleanly_delone_env, removes one element of the env_list wihout
    breaking the links. Needs to be fixed for the case where env is only one
    element (probably segfault case).
    
    Still needs to be thoroughly tested.
    Functions prototypes still need to be added to .h file.
    
    Changes to be committed:
    	modified:   sources/builtins/ms_unset.c
    	modified:   sources/env/ms_free_env.c

--- sources/builtins/ms_unset.c
+++ sources/builtins/ms_unset.c
@@ -6,13 +6,39 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 17:58:52 by aweaver           #+#    #+#             */
-/*   Updated: 2022/05/12 18:14:35 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/05/13 16:09:10 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_env *ft_find_env_elem(t_env *env_list, char *name)
+/* ************************************************************************ */
+/*	ACT: checks if str is a valid env variable								*/
+/*	ARG: a char *str														*/
+/*	RET: 1 if true 0 if false												*/
+/* ************************************************************************ */
+
+int	ft_is_valid_env_variable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_is_alnum(str[i] == 1) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/* ************************************************************************ */
+/*	ACT: finds the element of the list with env->name == param				*/
+/*	ARG: the env as a list, a char *name									*/
+/*	RET: returns the address of the element seeked for						*/
+/* ************************************************************************ */
+
+t_env	*ft_find_env_elem(t_env *env_list, char *name)
 {
 	while (env_list)
 	{
@@ -22,7 +48,31 @@ t_env *ft_find_env_elem(t_env *env_list, char *name)
 	}
 }
 
-t_env	*ft_unset(t_env *env_list, char *name)
+/* ************************************************************************ */
+/*	ACT: replicate unset function behaviour									*/
+/*	ARG: the env as a list, several char * as param names					*/
+/*	RET: void but prints error if incorrect format for names given			*/
+/* ************************************************************************ */
+
+void	*ft_unset(t_env *env_list, char *name, ...)
 {
-	
+	t_env	*target;
+	char	*target_name;
+	va_list	to_erase;
+
+	va_start(to_erase, name);
+	while (to_erase)
+	{
+		target_name = (char *) va_arg(to_erase);
+		if (ft_is_valid_env_variable(target_name) == 1)
+		{
+			target = ft_find_env_elem(env_list, target_name);
+			if (target != 0)
+				ft_cleanly_delone_env(&env_list, target);
+		}
+		else
+			fprintf(stderr, "unset: %s: invalid parameter name", name);
+		to_erase++;
+	}
+	va_end(to_erase);
 }
--- sources/env/ms_free_env.c
+++ sources/env/ms_free_env.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:27:33 by aweaver           #+#    #+#             */
-/*   Updated: 2022/05/12 15:21:20 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/05/13 15:59:57 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,44 @@ void	ft_free_double_array(char **str)
 }
 
 /* ************************************************************************ */
+/*	ACT : deletes an element (t_env) of a t_env list saving the list pointer*/
+/*	ARG : the address of the first element of the list and the address of 	*/
+/*	the element meant to be removed											*/
+/*	RET : void																*/
+/* ************************************************************************ */
+
+//one case needs to be addressed if env is composed of only 1 element.
+void	ft_cleanly_delone_env(t_env **env_start, t_env *target)
+{
+	t_env	*env_list;
+	t_env	*prev;
+
+	env_list = *env_start;
+	env_prev = *env_start;
+	while (env_list)
+	{
+		if (env_list == target)
+		{
+			if (env_list == *env_start)
+				*env_start = env_list->next;
+			else if (env_list->next)
+				env_prev->next = env_list->next;
+			else if (env_list->next == NULL)
+				env_prev->next = NULL;
+			ft_env_delone(env_list, free);
+			return ;
+		}
+		env_prev = env_list;
+		env_list = next;
+	}
+}
+
+/* ************************************************************************ */
 /*	ACT : deletes an element (t_env) of a t_env list breaking the list		*/
 /*	ARG : a t_env and a function returning void and taking a void pointer	*/
 /*	RET : void																*/
 /* ************************************************************************ */
+
 void	ft_env_delone(t_env *env, void (*del)(void *))
 {
 	int	i;
