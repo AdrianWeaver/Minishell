76cae08cacbdca1b854f520a8da2df99bb7fa003
aweaver <88249907+AdrianWeaver@users.noreply.github.com>
2022-06-17 13:29:04 +0200

    UPDATE PARSING:
    Squashed commit of the following:
    
    commit 720ba24b4fcd05677a79275ddcc678fd56e024f9
    Author: Adrian WEAVER <aweaver@student.42.fr>
    Date:   Fri Apr 15 09:55:38 2022 +0200
    
        FIX DIRECTORY CONFIGURATION:
    
        This commit is a major change in the directories structure
        it sets the files in the appropriate directories following the Makefile
        rework's.
    
        It may lead to errors in the following merge, be advised.
    
        Changes to be committed:
            renamed:    sources/ms_errors.c -> sources/errors/ms_errors.c
            renamed:    sources/ms_parser.c -> sources/parsing/ms_parser.c
            renamed:    sources/ms_args_lst_utils.c -> sources/utils/ms_args_lst_utils.c
            renamed:    sources/ms_mlc_lst_utils.c -> sources/utils/ms_mlc_lst_utils.c
    
    commit fe4e35a0aeac3d09b57d0971cc4079efce5da353
    Author: Jean-michel CERVONI <jcervoni@student.42.fr>
    Date:   Thu Apr 14 17:01:43 2022 +0200
    
        UPDATE MINISHEL.H :
    
        Update new functions prototypes (parsing);
    
        On branch jmc/parsing
        Changes to be committed:
            modified:   includes/minishell.h
    
    commit 10ea84d9ed941bbf7700e21a862c3957ff3177da
    Author: Jean-michel CERVONI <jcervoni@student.42.fr>
    Date:   Thu Apr 14 16:59:43 2022 +0200
    
        ADD MS_PARSE_TYPE.C :
    
        Add new parsing file ms_parse_type.c : will contain new parsing
        functions as redirection checker, is_file checker...;
    
        On branch jmc/parsing
        Changes to be committed:
            new file:   sources/parsing/ms_parse_type.c
    
    commit 030c5cabd1d3806dd0c5d18dfef0c1d708a2cd60
    Author: Jean-michel CERVONI <jcervoni@student.42.fr>
    Date:   Thu Apr 14 16:58:28 2022 +0200
    
        UPDATE MAIN.C :
    
        Update main.c : normed, still testing file;
    
        On branch jmc/parsing
        Changes to be committed:
            modified:   sources/main.c
    
    commit 50c5cf6161cc3c41614032dbb56155f1ba5e9e38
    Author: Jean-michel CERVONI <jcervoni@student.42.fr>
    Date:   Thu Apr 14 16:57:11 2022 +0200
    
        UPDATE MAKEFILE :
    
        Update Makefile : add newly created functions' prototype as
        ft_get_quote_arg, ft_get_arg, ft_custom_calloc;
    
    commit 17cd6844e48e8a822d2e32486e6a941e83bfa7b3
    Author: Jean-michel CERVONI <jcervoni@student.42.fr>
    Date:   Thu Apr 14 16:54:25 2022 +0200
    
        ADD FT_CUSTOM_CALLOC :
    
        Add ft_custom_calloc function which manage the memory allocation and
        create list of malloc'd address
        Doesn't work, need to call ft_custom_calloc in the libft functions
        instead of malloc;
    
        On branch jmc/parsing
        Changes to be committed:
            new file:   errors/ms_errors.c
    
    commit c17fd1836f69a5aec13b9febe7fddaeccbb713a7
    Author: Jean-michel CERVONI <jcervoni@student.42.fr>
    Date:   Thu Apr 14 16:52:44 2022 +0200
    
        UPDATE UTILS/ :
    
        Update utils folder : place files in right folder;
    
        Changes to be committed:
            new file:   utils/ms_args_lst_utils.c
            new file:   utils/ms_mlc_lst_utils.c
    
    commit 7c67a467d0d29ab3a781dd01fcfaea3cd7dac0a1
    Author: Jean-michel CERVONI <jcervoni@student.42.fr>
    Date:   Thu Apr 14 16:47:53 2022 +0200
    
        ADD FT_GET_ARG FT_GET_QUOTE_ARG :
    
        Add two parsing functions which return a pointenr to a list of
        arguments. Return NULL if failed;
        Normed file ms_parser.c;
    
        On branch jmc/parsing
        Changes to be committed:
            new file:   parsing/ms_parser.c
    
    Co-authored-by: Adrian WEAVER <aweaver@student.42.fr>
    Co-authored-by: Jean-michel CERVONI <jcervoni@student.42.fr>

--- sources/parsing/ms_parse_type.c
+++ sources/parsing/ms_parse_type.c
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ms_parse_type.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/08 09:18:40 by aweaver           #+#    #+#             */
-/*   Updated: 2022/04/14 16:45:39 by jcervoni         ###   ########.fr       */
+/*   Created: 2022/04/14 16:46:46 by jcervoni          #+#    #+#             */
+/*   Updated: 2022/04/14 16:46:55 by jcervoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int	main(int ac, char *av[], char *envp[])
-{
-	char	*test;
-	t_arg	*verif;
-
-	(void)envp;
-	(void)ac;
-	(void)av;
-	test = readline("Test readline :");
-	verif = ft_get_args(test);
-	ft_set_token(verif);
-	while (verif != NULL)
-	{
-		printf("content = %s,  token = %s\n", verif->content, verif->token);
-		verif = verif->next;
-	}
-	return (0);
-}
