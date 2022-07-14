aaf6984529d8b94546fe9ef5098bbc5bb326fbb8
Adrian WEAVER <aweaver@student.42.fr>
2022-07-14 12:00:16 +0200

    FIX PRINT_ENV:
    Fixing print_env, no longer prints empty variables
    
    Fix #19

--- sources/env/ms_print_env.c
+++ sources/env/ms_print_env.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@42.fr>                     +#+  +:+       +#+       */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 14:29:57 by aweaver           #+#    #+#             */
-/*   Updated: 2022/05/19 11:41:43 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/07/14 11:59:54 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ void	ft_print_env(t_env *env_list)
 {
 	while (env_list)
 	{
-		ft_printf("%s=", env_list->name);
-		ft_printf("%s\n", env_list->content);
+		if (env_list->content)
+		{
+			if (env_list->content[0] != 0)
+			{
+				ft_printf("%s=", env_list->name);
+				ft_printf("%s\n", env_list->content);
+			}
+		}
 		env_list = env_list->next;
 	}
 }
