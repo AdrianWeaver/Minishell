03338ab1b90ef44ff9b2bb313b078b551dca6f3d
Adrian WEAVER <aweaver@student.42.fr>
2022-08-01 12:22:42 +0200

    FIX RELATIVE PATH DISPLAY IN PROMPT:
    Fixing relative path display in prompt.
    Cd was broken because of this and resulted in incorrect free/corrupted
    data.
    
    Rebuilt the functions to suit the needs of the project.
    
    Changes to be committed:
    	modified:   sources/utils/ms_prompt.c

--- sources/utils/ms_prompt.c
+++ sources/utils/ms_prompt.c
@@ -6,7 +6,7 @@
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 11:35:51 by jcervoni          #+#    #+#             */
-/*   Updated: 2022/08/01 09:27:05 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/08/01 12:21:23 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,34 +32,84 @@ char	*ft_get_prompt(int std[2])
 	return (name);
 }
 
+int	ft_check_for_home(char *cwd)
+{
+	int	i;
+
+	i = 0;
+	if (cwd == NULL)
+	{
+		rl_clear_history();
+		ft_eprintf(MS_NOCWD);
+		ft_magic_malloc(FLUSH, 1, NULL);
+	}
+	while (cwd[i] && ft_strncmp(&cwd[i], "home", 4) != 0)
+		i++;
+	if (cwd[i] == '\0')
+		return (0);
+	i += 4;
+	if (cwd[i] == 's')
+		i++;
+	if (cwd[i] == '/')
+		i++;
+	else
+		return (0);
+	while (cwd[i] && cwd[i] != '/')
+		i++;
+	if (cwd[i] == '/')
+		i++;
+	return (i);
+}
+
 char	*ft_get_short_path(char *cwd)
 {
-	char	*path;
-	char	*tmp;
 	int		i;
-	int		j;
+	char	*tmp_short_cwd;
+	char	*short_cwd;
 
-	path = NULL;
-	tmp = NULL;
-	i = 6;
-	j = 0;
-	while (ft_strncmp(&cwd[j], "home", 4) != 0)
-		j++;
-	path = ft_strdup(&cwd[j + i]);
-	ft_magic_malloc(ADD, 0, path);
-	if (path)
+	i = ft_check_for_home(cwd);
+	short_cwd = NULL;
+	if (i == 0)
+		return (ft_strdup(cwd));
+	if (i > 0)
 	{
-		while (path[i] != '/')
-			i++;
-		tmp = ft_strdup(&path[i + 1]);
-		ft_magic_malloc(ADD, 0, tmp);
-		path = ft_magic_malloc(FREE, 0, path);
-		path = ft_strjoin("~/", tmp);
-		tmp = ft_magic_malloc(FREE, 0, NULL);
-		return (path);
+		tmp_short_cwd = ft_strdup(&cwd[i]);
+		short_cwd = ft_magic_malloc(ADD, 0, ft_strjoin("~", tmp_short_cwd));
+		free(tmp_short_cwd);
+		tmp_short_cwd = NULL;
 	}
-	return (cwd);
+	return (short_cwd);
 }
+
+//char	*ft_get_short_path(char *cwd)
+//{
+	//char	*path;
+	//char	*tmp;
+	//int		i;
+	//int		j;
+//
+	//path = NULL;
+	//tmp = NULL;
+	//i = 6;
+	//j = 0;
+	//ft_eprintf("CWD = %s\n", cwd);
+	//while (ft_strncmp(&cwd[j], "homes", 5) != 0)
+		//j++;
+	//path = ft_strdup(&cwd[j + i]);
+	//ft_magic_malloc(ADD, 0, path);
+	//if (path)
+	//{
+		//while (path[i] != '/')
+			//i++;
+		//tmp = ft_strdup(&path[i + 1]);
+		//ft_magic_malloc(ADD, 0, tmp);
+		//path = ft_magic_malloc(FREE, 0, path);
+		//path = ft_strjoin("~/", tmp);
+		//tmp = ft_magic_malloc(FREE, 0, NULL);
+		//return (path);
+	//}
+	//return (cwd);
+//}
 
 char	*ft_display_prompt(int std[2])
 {
